33189d638b49347854df3c0ba5ded44eafc405e4
guilhermeperottoni <50721664+guilhermeperottoni@users.noreply.github.com>
2021-03-09 20:54:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express'); 
+
+const app = express();
+
+app.get('/', (request, response) =>{
+    return response.json({message: 'Hello Podi'});
+
+})
+
+app.listen(3333);
+
+
+    
